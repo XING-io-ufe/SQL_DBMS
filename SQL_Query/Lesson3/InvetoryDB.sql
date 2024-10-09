da77ccf8fe1327a2bce39f9cdc3bc417ff520413
Odokaa (オドカー) <xingdaiyamondo@gmail.com>
2024-10-09 18:00:10 +0800

    update

--- SQL_Query/Lesson3/InvetoryDB.sql
+++ SQL_Query/Lesson3/InvetoryDB.sql
@@ -1,61 +1,61 @@
-CREATE TABLE Category (
-	CategoryID INT PRIMARY KEY,
-	CategoryName VARCHAR(100) NOT NULL
-);
+-- CREATE TABLE Category (
+-- 	CategoryID INT PRIMARY KEY,
+-- 	CategoryName VARCHAR(100) NOT NULL
+-- );
 
 
-SELECT * FROM Category;
+-- SELECT * FROM Category;
 
-CREATE TABLE Products (
-	ProductsID INT PRIMARY KEY,
-	ProductsName VARCHAR(255) NOT NULL,
-	CategotyID INT,
-	Price DECIMAL(10, 2) NOT NULL,
-	Quantity VARCHAR(100)
-);
+-- CREATE TABLE Products (
+-- 	ProductsID INT PRIMARY KEY,
+-- 	ProductsName VARCHAR(255) NOT NULL,
+-- 	CategotyID INT,
+-- 	Price DECIMAL(10, 2) NOT NULL,
+-- 	Quantity VARCHAR(100)
+-- );
 
-ALTER TABLE Category ADD SupplierID INT;
+-- ALTER TABLE Category ADD SupplierID INT;
 
-ALTER TABLE Products ALTER COLUMN Quantity TYPE DECIMAL(12, 2) USING Quantity::DECIMAL(12, 2);
+-- ALTER TABLE Products ALTER COLUMN Quantity TYPE DECIMAL(12, 2) USING Quantity::DECIMAL(12, 2);
 
-SELECT Quantity FROM Products;
+-- SELECT Quantity FROM Products;
 
-INSERT INTO  Category (CategoryID, CategoryName, SupplierID) 
-VALUES 
-	(1, 'Electronics', 1),
-	(2, 'Clothing', 1);
+-- INSERT INTO  Category (CategoryID, CategoryName, SupplierID) 
+-- VALUES 
+-- 	(1, 'Electronics', 1),
+-- 	(2, 'Clothing', 1);
 
-SELECT * FROM Category;
+-- SELECT * FROM Category;
 
-SELECT * FROM Products;
+-- SELECT * FROM Products;
 
-INSERT INTO Products (ProductsID, ProductsName, CategotyID, Price, Quantity)
-VALUES
-	(1, 'ProducstA', 1, 10.99, 50),
-	(2, 'ProducstB', 2, 19.99, 30),
-	(3, 'ProducstC', 1, 15.49, 20),
-	(4, 'ProducstD', 2, 24.99, 40);
+-- INSERT INTO Products (ProductsID, ProductsName, CategotyID, Price, Quantity)
+-- VALUES
+-- 	(1, 'ProducstA', 1, 10.99, 50),
+-- 	(2, 'ProducstB', 2, 19.99, 30),
+-- 	(3, 'ProducstC', 1, 15.49, 20),
+-- 	(4, 'ProducstD', 2, 24.99, 40);
 
-SELECT * FROM Products WHERE CategotyID = 2;
+-- SELECT * FROM Products WHERE CategotyID = 2;
 
-SELECT * FROM Products WHERE Price > 15
+-- SELECT * FROM Products WHERE Price > 15
 
-UPDATE Products
-SET Quantity = Quantity - 10 
-WHERE ProductsName = 'ProducstC';
+-- UPDATE Products
+-- SET Quantity = Quantity - 10 
+-- WHERE ProductsName = 'ProducstC';
 
-SELECT * FROM Products;
+-- SELECT * FROM Products;
 
-DELETE FROM Products
-WHERE ProductsName = 'ProducstB';
+-- DELETE FROM Products
+-- WHERE ProductsName = 'ProducstB';
 
-TRUNCATE TABLE Products;
+-- TRUNCATE TABLE Products;
 
-TRUNCATE TABLE Category;
+-- TRUNCATE TABLE Category;
 
-DROP TABLE Products;
+-- DROP TABLE Products;
 
-DROP TABLE Category;
+-- DROP TABLE Category;
 
 
 
--- SQL_Query/Lesson4/Books.sql
+++ SQL_Query/Lesson4/Books.sql
@@ -1,52 +1,52 @@
-CREATE TABLE Books (
-	Book_ID INT PRIMARY KEY,
-	Title VARCHAR NOT NULL,
-	ISBN VARCHAR(100) UNIQUE,
-	Published_YEAR DATE NOT NULL,
-	Genre VARCHAR,
-	Quantity_In_Stock INT NOT NULL,
-	Price INT NOT NULL
-);
+-- CREATE TABLE Books (
+-- 	Book_ID INT PRIMARY KEY,
+-- 	Title VARCHAR NOT NULL,
+-- 	ISBN VARCHAR(100) UNIQUE,
+-- 	Published_YEAR DATE NOT NULL,
+-- 	Genre VARCHAR,
+-- 	Quantity_In_Stock INT NOT NULL,
+-- 	Price INT NOT NULL
+-- );
 
-CREATE TABLE Authors(
-	Author_ID INT PRIMARY KEY,
-	First_Name VARCHAR NOT NULL,
-	Last_Name VARCHAR NOT NULL,
-	Birth_Year DATE NOT NULL,
-	Nationality VARCHAR
-);
+-- CREATE TABLE Authors(
+-- 	Author_ID INT PRIMARY KEY,
+-- 	First_Name VARCHAR NOT NULL,
+-- 	Last_Name VARCHAR NOT NULL,
+-- 	Birth_Year DATE NOT NULL,
+-- 	Nationality VARCHAR
+-- );
 
-INSERT INTO Books (Book_ID, Title, ISBN, Published_YEAR, Genre, Quantity_In_Stock, Price)
-VALUES
-    (1, 'ADAMS', '9780061120084', '1960-07-11', 'Fiction', 10, 20),
-    (2, '1984', '9780451524935', '1949-06-08', 'Dystopian', 5, 15),
-    (3, 'The Great Gatsby', '9780743273565', '1925-04-10', 'Classic', 8, 18),
-    (4, 'Pride and Prejudice', '9780486280511', '1813-01-28', 'Romance', 12, 22),
-    (5, 'The Catcher in the Rye', '9780316769174', '1951-07-16', 'Young Adult', 7, 19);
+-- INSERT INTO Books (Book_ID, Title, ISBN, Published_YEAR, Genre, Quantity_In_Stock, Price)
+-- VALUES
+--     (1, 'ADAMS', '9780061120084', '1960-07-11', 'Fiction', 10, 20),
+--     (2, '1984', '9780451524935', '1949-06-08', 'Dystopian', 5, 15),
+--     (3, 'The Great Gatsby', '9780743273565', '1925-04-10', 'Classic', 8, 18),
+--     (4, 'Pride and Prejudice', '9780486280511', '1813-01-28', 'Romance', 12, 22),
+--     (5, 'The Catcher in the Rye', '9780316769174', '1951-07-16', 'Young Adult', 7, 19);
 
-INSERT INTO Authors (Author_ID, First_Name, Last_Name, Birth_Year, Nationality)
-VALUES
-    (1, 'Harper', 'Lee', '1926-04-28', 'American'),
-    (2, 'George', 'Orwell', '1903-06-25', 'British'),
-    (3, 'F. Scott', 'Fitzgerald', '1896-09-24', 'American'),
-    (4, 'Jane', 'Austen', '1775-12-16', 'British'),
-    (5, 'J.D.', 'Salinger', '1919-01-01', 'American');
+-- INSERT INTO Authors (Author_ID, First_Name, Last_Name, Birth_Year, Nationality)
+-- VALUES
+--     (1, 'Harper', 'Lee', '1926-04-28', 'American'),
+--     (2, 'George', 'Orwell', '1903-06-25', 'British'),
+--     (3, 'F. Scott', 'Fitzgerald', '1896-09-24', 'American'),
+--     (4, 'Jane', 'Austen', '1775-12-16', 'British'),
+--     (5, 'J.D.', 'Salinger', '1919-01-01', 'American');
 	
-SELECT * FROM Books WHERE Published_YEAR <= '2010-01-01';
+-- SELECT * FROM Books WHERE Published_YEAR <= '2010-01-01';
 
-SELECT FIRST_NAME, LAST_NAME FROM Authors WHERE Birth_year < '1990-01-01' ORDER BY First_Name ASC;
+-- SELECT FIRST_NAME, LAST_NAME FROM Authors WHERE Birth_year < '1990-01-01' ORDER BY First_Name ASC;
 
-SELECT COUNT(*) FROM Books WHERE Price > 10 ;
+-- SELECT COUNT(*) FROM Books WHERE Price > 10 ;
 
-SELECT Title FROM Books WHERE Title LIKE 'T%';
+-- SELECT Title FROM Books WHERE Title LIKE 'T%';
 
-SELECT Title FROM Books WHERE Published_YEAR BETWEEN '1924-01-01' AND '1961-01-01';
+-- SELECT Title FROM Books WHERE Published_YEAR BETWEEN '1924-01-01' AND '1961-01-01';
 
-UPDATE Books SET Genre='Young Adult' WHERE Book_ID =1;
-SELECT * FROM Books;
+-- UPDATE Books SET Genre='Young Adult' WHERE Book_ID =1;
+-- SELECT * FROM Books;
 
-SELECT Genre, COUNT(*) FROM Books GROUP BY Genre HAVING COUNT(*) > 1;
+-- SELECT Genre, COUNT(*) FROM Books GROUP BY Genre HAVING COUNT(*) > 1;
 
-ALTER TABLE Books ADD CONSTRAINT FK_Authors_ID FOREIGN KEY(Book_ID) REFERENCES Authors(Author_ID);
+-- ALTER TABLE Books ADD CONSTRAINT FK_Authors_ID FOREIGN KEY(Book_ID) REFERENCES Authors(Author_ID);
 
-SELECT * FROM Books;
+-- SELECT * FROM Books;
